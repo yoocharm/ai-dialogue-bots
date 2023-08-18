3a81a35565b4cfbee55343c8e780bd85fcc79635
yoocharm <160447066+yoocharm@users.noreply.github.com>
2023-08-18 18:28:23 +0000

    improving error handling

--- rustbot/src/audio.rs
+++ rustbot/src/audio.rs
@@ -66,4 +66,10 @@ pub async fn play(
                     sink.sleep_until_end();
                     // NOTE: notify jet::writer
                     audio_done.send(true)?;
-                    has_played_audio = false
+                    has_played_audio = false;
+                }
+            }
+        }
+    }
+
+    // Flush any remainin
