9a907ca882483dd4daefef99037a4a9bec01d201
yoocharm <160447066+yoocharm@users.noreply.github.com>
2023-09-17 11:46:59 +0000

    Fix comment.

--- rustbot/src/llm.rs
+++ rustbot/src/llm.rs
@@ -3,4 +3,8 @@ use bytes::Bytes;
 use ollama_rs::{generation::completion::request::GenerationRequest, Ollama};
 use tokio::{
     self,
-    sync::mpsc::{Receiver, Sender
+    sync::mpsc::{Receiver, Sender},
+    sync::watch,
+    task::JoinHandle,
+};
+use tokio_stream::Str
