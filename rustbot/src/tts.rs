d6c442317c95b869ebd8724936df4a9896357201
yoocharm <160447066+yoocharm@users.noreply.github.com>
2024-02-10 18:58:43 +0000

    added files

--- rustbot/src/tts.rs
+++ rustbot/src/tts.rs
@@ -1,3 +1,6 @@
 use crate::{buffer, prelude::*};
 use bytes::Bytes;
-use playht_rs::api::{self, stream::TTSStreamReq, tts::Quality
+use playht_rs::api::{self, stream::TTSStreamReq, tts::Quality};
+use tokio::{self, sync::mpsc::Receiver, sync::watch};
+
+#[derive(De
